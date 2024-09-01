a3ad73b17454d0bac13833e7b10de9fc1cd39d9d
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-09-01 11:40:50 -0300

    feat: :sparkles: add sign in page initial structure and react hook form

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,21 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Autenticação</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">Pizza Shop</span>
+        </div>
 
-      <div>
+        <footer>
+          Painel do parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,58 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const SignInFormSchema = z.object({
+  email: z.string().email(),
+});
+
+type SignInFormData = z.infer<typeof SignInFormSchema>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(SignInFormSchema),
+  });
+
+  async function handleSignIn(data: SignInFormData) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  }
+
   return (
-    <div className="flex h-screen items-center justify-center">
+    <>
       <Helmet title="Login" />
-      <h1>Sign In</h1>
-    </div>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel parceiro
+            </p>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit(handleSignIn)} className="mt-6 space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" required {...register("email")} />
+          </div>
+
+          <Button disabled={isSubmitting} type="submit" className="w-full">
+            Acessar painel
+          </Button>
+        </form>
+      </div>
+    </>
   );
 }
