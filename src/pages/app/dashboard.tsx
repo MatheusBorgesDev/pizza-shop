d0675b1609112e5a2e7e4702133fcb6d1216d214
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-09-02 18:10:10 -0300

    chore: :recycle: ensure dashboard content has full height

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet-async";
 
 export function Dashboard() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex flex-1 items-center justify-center">
       <Helmet title="Dashboard" />
       <h1>Dashboard</h1>
     </div>
