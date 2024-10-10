2c2bdf46d359202f609ed691cbdda4395926f8cb
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-10-09 22:09:12 -0300

    refactor: :recycle: rename mock server files

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,15 +2,15 @@ import { setupWorker } from "msw/browser";
 
 import { env } from "@/env";
 
-import { getGetDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period";
-import { getDayOrdersAmountMock } from "./get-day-orders-amount";
+import { getGetDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
+import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
 import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
-import { getMonthcanceledOrdersAmountMock } from "./get-month-canceled-orders-amount";
-import { getMonthOrdersAmountMock } from "./get-month-orders-amount";
-import { getMonthrevenueMock } from "./get-month-revenue";
+import { getMonthcanceledOrdersAmountMock } from "./get-month-canceled-orders-amount-mock";
+import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
+import { getMonthrevenueMock } from "./get-month-revenue-mock";
 import { getOrderDetailsMock } from "./get-order-details-mock";
 import { getOrdersMock } from "./get-orders-mock";
-import { getPopularProductsMock } from "./get-popular-products";
+import { getPopularProductsMock } from "./get-popular-products-mock";
 import { getProfileMock } from "./get-profile-mock";
 import { registerRestaurantMock } from "./register-restaurant-mock";
 import { signInMock } from "./sign-in-mock";
