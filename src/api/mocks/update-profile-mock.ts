ddb6c77d7214906f8fa3857b1166f7daf2354a79
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-10-17 23:18:18 -0300

    feat: :white_check_mark: add e2e tests for updating profile

--- src/api/mocks/update-profile-mock.ts
+++ src/api/mocks/update-profile-mock.ts
@@ -7,7 +7,7 @@ export const updateProfileMock = http.put<never, UpdateProfileBody>(
   async ({ request }) => {
     const { name } = await request.json();
 
-    if (name === "Rocket Pizza") {
+    if (name === "New Valid Name") {
       return new HttpResponse(null, { status: 204 });
     }
 
---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "@playwright/test";
+
+test("update profile succesfully", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  await page.getByRole("button", { name: "Pizza Shop" }).click();
+  await page.getByRole("menuitem", { name: "Perfil da loja" }).click();
+  await page.getByLabel("Nome").fill("New Valid Name");
+  await page.getByLabel("Descrição").fill("New restaurant description");
+  await page.getByRole("button", { name: "Salvar" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  const toast = page.getByText("Perfil atualizado com sucesso!");
+
+  expect(toast).toBeVisible();
+
+  await page.getByRole("button", { name: "Close" }).click();
+
+  const restaurantName = await page.getByText("New Valid Name");
+
+  expect(restaurantName).toBeVisible();
+});
